ab3ef3187210cdd4092da9fe2ae4ef50cc858e6d
Andre Nathan <andre@digirati.com.br>
2007-08-13 15:46:21 +0000

    Update the simple exception test.

--- test/ml/test.ml
+++ test/ml/test.ml
@@ -1,9 +1,6 @@
-let test_send_fd =
+let test_send_fd () =
   let f = Unix.openfile "Makefile" [Unix.O_RDONLY] 640 in
   try Fd.send_fd f f with
-  | Unix.Unix_error (code, f, x) ->
-      Printf.printf "%s: %s\n" f (Unix.error_message code)
-  | Invalid_argument err ->
-      Printf.printf "%s\n" err
+  | Fd.Fd_error s -> Printf.printf "Fd.send_fd: %s\n" s
 
-let _ = test_send_fd
+let _ = test_send_fd ()
